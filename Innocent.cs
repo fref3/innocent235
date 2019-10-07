ee71e3c7f4a54b097124b2d350b265be6295bd8d
Philip <phileli@dinfo.cegepthetford.ca>
2019-10-06 20:58:31 -0400

    Ajout de la classe Inoncent (C#)

---
+++ Innocent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Innoncent
+{
+    private string _nom;
+	public Innoncent()
+	{
+        _nom = "Jean-Francois";
+	}
+    public void direSalutPhilip()
+    {
+        Console.WriteLine("SALUT PHILIP");
+    }
+}
